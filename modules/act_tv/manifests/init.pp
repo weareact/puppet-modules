efd6ffb3d85be77d42261d0b40e2422c6c0e6fb5
alex-snape <alex.snape@weareact.com>
2016-08-15 14:31:20 +0100

    Updated

--- modules/act_tv/manifests/init.pp
+++ modules/act_tv/manifests/init.pp
@@ -27,12 +27,43 @@ class act_tv {
 		require =>  Class['wso2am']
 	}
 
+	$actdev_trust = "${security_path}/client-truststore.jks"
+
+	file { 'trust-store':
+		path	=>	$actdev_trust,
+		source	=>  'puppet:///modules/act_tv/client-truststore.jks',
+		owner   =>  'wso2user',
+		group	=>	'wso2',
+		require =>  Class['wso2am']
+	}
+
 	$conf_path = "${repository_path}/conf"
 	$claim_config = "${conf_path}/claim-config.xml"
 
 	file { 'claim-config':
 		path	=>	$claim_config,
 		source	=>  'puppet:///modules/act_tv/claim-config.xml',
+		owner   =>  'wso2user',
+		group	=>	'wso2',
+		require =>  Class['wso2am']
+	}
+
+	$database_path = "${repository_path}/database"
+	$h2 = "${database_path}/weareact_com.h2.db"
+
+	file { 'secondary-user-h2':
+		path	=>	$h2,
+		source	=>  'puppet:///modules/act_tv/weareact_com.h2.db',
+		owner   =>  'wso2user',
+		group	=>	'wso2',
+		require =>  Class['wso2am']
+	}
+
+	$h2_trace = "${database_path}/weareact_com.trace.db"
+
+	file { 'secondary-user-h2-trace':
+		path	=>	$h2_trace,
+		source	=>  'puppet:///modules/act_tv/weareact_com.trace.db',
 		owner   =>  'wso2user',
 		group	=>	'wso2',
 		require =>  Class['wso2am']
